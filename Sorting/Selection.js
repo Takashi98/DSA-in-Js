284185b28a0a9d3aa0e8a2cd9461ae092b991f2e
Takashi98 <vishalprjpt15@gmail.com>
2025-01-16 23:27:36 +0530

    we need to swap when min not equal to i index(optimization)

--- Sorting/Selection.js
+++ Sorting/Selection.js
@@ -16,4 +16,26 @@ function SelectionSort(array){
     return array
 }
 
+
+
+
+function SelectionSortwithSomeCorrection(array){
+    if(array.length === 0) return false
+    for(let i=0; i<array.length-1; i++){
+        let min = i
+        for(let j=i+1; j<array.length-1; j++){
+            if(array[j]<array[min]){
+                array[j] = min
+            }
+        }
+        // swap 
+        if(min !== i){
+        let temp = array[i]
+        array[i] = array[min]
+        array[min] = temp
+        }
+    }
+    return array
+}
+
 console.log(SelectionSort([3,1,5,6,7]))
