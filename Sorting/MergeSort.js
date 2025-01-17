9d67e596195ae9a9032b9fd0339c8a791c5b9542
Takashi98 <vishalprjpt15@gmail.com>
2025-01-18 00:22:20 +0530

    Merge sort

--- Sorting/MergeSort.js
+++ Sorting/MergeSort.js
@@ -3,22 +3,42 @@
 
 // first writting the code for merging two sorted array
 
-function MergeTwoSortedArray(array1, array2){
-    if(array1.length && array2.length === 0) return false
+function MergeSort(array){
+    if(array.length <= 1) return array
+
+    let mid = Math.floor(array.length/2)
+    let left = array.slice(0,mid)
+    let right = array.slice(mid)
+
+    let sortedLeft = MergeSort(left)
+    let sortedRight = MergeSort(right)
+
+    // merge two sorted array
+    // console.log(sortedLeft, sortedRight)
+
+    let fuckMe = MergeSorted(sortedLeft,sortedRight)
+
+
+    return fuckMe
+
+}
+
+function MergeSorted(array1, array2){
+    if(array1.length && array2.length ===0) return []
     let result = []
-    let i = 0; 
-    let j = 0;
-    
-    while(i< array1.length && j<array2.length){
-        if(array2[j]> array1[i]){
+    let i =0;
+    let j=0;
+    while(i<array1.length && j<array2.length){
+        if(array2[j]>array1[i]){
             result.push(array1[i])
             i++
-        }else{
+        }else {
             result.push(array2[j])
             j++
         }
     }
-    while(i<array1.length){
+
+    while(i< array1.length){
         result.push(array1[i])
         i++
     }
@@ -28,6 +48,8 @@ function MergeTwoSortedArray(array1, array2){
     }
 
     return result
+
 }
 
-console.log(MergeTwoSortedArray([1,2,3],[4,5,6,7]))
+console.log(MergeSort([15,2,5,67,4,33,22,667]))
+
