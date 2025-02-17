4a1e678324fce2aea1763ba5e01d34928a98a1c4
Takashi98 <vishalprjpt15@gmail.com>
2025-02-18 03:30:28 +0530

    added pop method

--- DataStructure/linkedList.js
+++ DataStructure/linkedList.js
@@ -23,8 +23,33 @@ class SinglyLinkedList{
         this.length++;
         return this;
     }
+    pop(){
+        if(!this.head) return undefined;
+        let current = this.head;
+        let newTail = this.head;
+        while(current.next){
+            newTail = current;
+            current = current.next;
+        }
+        this.tail = newTail;
+        this.tail.next = null;
+        this.length--;
+        if(this.length ===0){
+            this.head = null;
+            this.tail = null;
+        }
+        return current;
+    }
 }
 
 let okay = new SinglyLinkedList;
 okay.push(4).push(5).push(6).push(7).push(8).push(9)
+console.log(okay);
+okay.pop();
+okay.pop();
+okay.pop();
+okay.pop();
+okay.pop();
+okay.pop();
+okay.pop();
 console.log(okay);
