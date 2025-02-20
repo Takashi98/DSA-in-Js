bd34d9ae7e450cc25021b35ec7e807c5019c2fe3
Takashi98 <vishalprjpt15@gmail.com>
2025-02-20 15:30:33 +0530

    added unshift and get method in doubly linkedlist

--- DataStructure/doublyLinkedList.js
+++ DataStructure/doublyLinkedList.js
@@ -15,10 +15,81 @@ class DoublyLinkedList{
 
  
     push(value){
-        let newnode = new Node(value);
+        let newNode = new Node(value);
         if(!this.head){
             this.head = newNode;
-            this.tail = newnode;
+            this.tail = newNode;
+        }else{
+            this.tail.next = newNode;
+            newNode.prev = this.tail;
+            this.tail =  newNode;
+        }
+        this.length++;
+        return this;
+    }
+
+    pop(){
+        if(!this.head) return undefined;
+        let removeNode = this.tail;
+        if(this.length ===1){
+            this.head = null;
+            this.tail = null;
+        }else{
+            this.tail = removeNode.prev;
+            this.tail.next = null;
+            removeNode.prev = null
+        }
+        this.length--;
+        return removeNode;
+    }
+
+    shift(){
+        if(!this.head) return undefined;
+        let removeVal = this.head;
+        if(this.length===1){
+            this.head = null;
+            this.tail = null;
+        }else{
+            this.head = removeVal.next;
+            this.head.prev = null;
+        }
+        this.length--;
+        return removeVal;
+    }
+
+    unshift(value){
+        let newNode = new Node(value);
+        if(!this.head){
+            this.head = newNode;
+            this.tail = newNode;
+        }else{
+            newNode.next = this.head;
+            this.head.prev = newNode;
+            this.head = newNode; 
+        }
+        this.length++;
+        return newNode;
+    }
+
+    get(index){
+        if(index<0 || index>= this.length) return undefined;
+        let counter =  0;
+        let current = this.head;
+        if(index<this.length/2){
+            while(counter!=index){
+                current = current.next;
+                counter++;
+            }
+            return current;
+        }else{
+            current = this.tail;
+            counter = this.length-1;
+            while(counter!=index){
+                current = current.prev;
+                counter--
+            }
+            return current;
         }
     }
 }
+      
