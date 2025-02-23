638fdc1b252cbcf25f4dfd69d8fd8bf8d3a8c3cb
Takashi98 <vishalprjpt15@gmail.com>
2025-02-23 18:34:50 +0530

    added find method

--- DataStructure/binarySearchTree.js
+++ DataStructure/binarySearchTree.js
@@ -10,31 +10,60 @@ class BinarySearchTree {
   constructor() {
     this.root = null;
   }
-
-  insert(value) {
+  
+  insert(value){
     let newNode = new Node(value);
-
-    if (!this.root) {
+    if(!this.root){
       this.root = newNode;
       return this;
     }
     let current = this.root;
-    while (true) {
-      if (value < current.value) {
-        if (!current.left) {
+    while(true){
+      if(value<current.value){
+        if(!current.left){
           current.left = newNode;
           return this;
         }else{
-            current = current.left;
+          current = current.left;
         }
       }else if(value > current.value){
         if(!current.right){
-            current.right = newNode;
-            return this;
+          current.right = newNode;
+          return this;
         }else{
-            current = current.right;
+          current = current.right;
         }
       }
     }
+     
+  }
+
+  find(value){
+    if(!this.root) return false;
+    let current = this.root;
+    while(current !== null){
+      if(value === current.value){
+        return true;
+      }else if(value < current.value){
+          current = current.left;
+      }else{
+        current = current.right;
+      }
+    }
+    return false;
   }
 }
+
+let tree = new BinarySearchTree;
+tree.insert(11);
+tree.insert(2);
+tree.insert(50);
+tree.insert(23);
+tree.insert(12);
+tree.insert(56);
+console.log(tree);
+
+tree.find(2);
+
+console.log(tree);
+
