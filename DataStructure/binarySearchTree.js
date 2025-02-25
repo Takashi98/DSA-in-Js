4118fe9f9ac16b32abf67020afe58116c95efcc3
Takashi98 <vishalprjpt15@gmail.com>
2025-02-25 15:49:56 +0530

    impliment DFS pre order and DFS post order

--- DataStructure/binarySearchTree.js
+++ DataStructure/binarySearchTree.js
@@ -10,101 +10,98 @@ class BinarySearchTree {
   constructor() {
     this.root = null;
   }
-  
-  insert(value){
+
+  insert(value) {
     let newNode = new Node(value);
-    if(!this.root){
+    if (!this.root) {
       this.root = newNode;
       return this;
     }
     let current = this.root;
-    while(true){
-      if(value<current.value){
-        if(!current.left){
+    while (true) {
+      if (value < current.value) {
+        if (!current.left) {
           current.left = newNode;
           return this;
-        }else{
+        } else {
           current = current.left;
         }
-      }else if(value > current.value){
-        if(!current.right){
+      } else if (value > current.value) {
+        if (!current.right) {
           current.right = newNode;
           return this;
-        }else{
+        } else {
           current = current.right;
         }
       }
     }
-     
   }
 
-  find(value){
-    if(!this.root) return false;
+  find(value) {
+    if (!this.root) return false;
     let current = this.root;
-    while(current !== null){
-      if(value === current.value){
+    while (current !== null) {
+      if (value === current.value) {
         return true;
-      }else if(value < current.value){
-          current = current.left;
-      }else{
+      } else if (value < current.value) {
+        current = current.left;
+      } else {
         current = current.right;
       }
     }
-    return false;  
-  } 
+    return false;
+  }
 
-  breadthFirstSearch(){
+  BreadthFirstSearch() {
     let queue = [];
     let result = [];
     let current = this.root;
-    if(!this.root) return [];
-    queue.push(current);
 
-    while(queue.length){
-      current = queue.shift()
+    queue.push(current);
+    while (queue.length) {
+      current = queue.shift();
       result.push(current);
-      if(current.left){
+
+      if (current.left) {
         queue.push(current.left);
+
+        if (current.rigth) {
+          queue.push(current.right);
+        }
       }
-      if(current.rigth){
-        queue.push(current.right)
-      }
+      return result;
     }
-    return result;
   }
-  
+
+ 
+  DFSPreOrder(){
+    let data = [];
+    let current = this.root;
+    
+    function traverse(node){
+      data.push(node);
+      if(node.left) traverse(node.left);
+      if(node.right) traverse(node.right);
+    }
+
+    traverse(current);
+    return data;
+  }
+
+  DFSPostOrder(){
+    let data = [];
+
+    function traverse(node){
+      if(node.left) traverse(node.left);
+      if(node.right) traverse(node.right);
+      data.push(node);
+    }
+    traverse(this.root);
+    return data;
+  }
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-let tree = new BinarySearchTree;
+let tree = new BinarySearchTree();
 tree.insert(11);
 tree.insert(2);
 tree.insert(50);
@@ -112,8 +109,3 @@ tree.insert(23);
 tree.insert(12);
 tree.insert(56);
 // console.log(tree);
-
-tree.find(2);
-
-console.log(tree);
-
