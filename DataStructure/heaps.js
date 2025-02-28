42b64ea13851c1fc7966d237ea1588dd55731a71
Takashi98 <vishalprjpt15@gmail.com>
2025-03-01 01:04:14 +0530

    added Max Extract Method

--- DataStructure/heaps.js
+++ DataStructure/heaps.js
@@ -1,25 +1,66 @@
-class MaxBinaryHeap{
-    constructor(){
-        this.value = [];
-    }
+class MaxBinaryHeap {
+  constructor() {
+    this.value = [];
+  }
 
-    insert(element){
-        this.value.push(element);
-        this.bubbleUp();
-    }
-    
-    bubbleUp(){
-        let index = this.value.length-1;
-        let element = this.value[index];
+  insert(element) {
+    this.value.push(element);
+    this.bubbleUp();
+  }
 
-        while(index>0){
-        let parentIdx = Math.floor((index-1)/2);
-        let parent = this.value[parentIdx];
-        if(element <= parent) break;
-        this.value[parentIdx] = element;
-        this.value[index] = parent;
-        index = parentIdx;
+  bubbleUp() {
+    let index = this.value.length - 1;
+    let element = this.value[index];
+
+    while (index > 0) {
+      let parentIndex = Math.floor((index - 1) / 2);
+      let parentValue = this.value[parentIndex];
+      if (element <= parentValue) break;
+      this.value[parentIndex] = element;
+      this.value[index] = parentValue;
+      index = parentIndex;
     }
+  }
+
+  ExtractMax(){
+    let max = this.value[0];
+    let end = this.value.pop();
+    this.value[0] = end;
+    this.sinkDown();
+    return max;
+
+  }
+  sinkDown(){
+    let idx = 0;
+    let length = this.value.length;
+    let element = this.value[idx];
+
+    while(true){
+        let leftChildIdx = 2 * idx + 1;
+        let rigthChildIdx = 2 * idx + 2;
+        let leftChild,rightChild;
+        let swap = null;
+        if(leftChildIdx < length){
+            leftChild = this.value[leftChildIdx];
+            if(leftChild > element){
+                swap =  leftChildIdx;
+            }
+        }
+        if(rigthChildIdx < length) {
+            rightChild = this.value[rigthChildIdx];
+            if(
+                (swap === null && rightChild>element) ||
+                (swap !== null && rightChild>leftChild)
+            ){
+                swap = rigthChildIdx
+            }
+        }
+
+        if(swap === null) return null;
+        this.value[idx] = this.value[swap];
+        this.value[swap] = element;
+        idx = swap;
 
     }
+  }
 }
