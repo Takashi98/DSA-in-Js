a3f0b682af784ad9708caa2f49c27782a5fbe1c9
Takashi98 <vishalprjpt15@gmail.com>
2025-03-03 16:40:10 +0530

    A HashTable Class

---
+++ DataStructure/hashTable.js
@@ -0,0 +1,17 @@
+class hashTable{
+    constructor(size = 53){
+        this.keyMap = new Array(size);
+    }
+
+    _hash(key){
+        let total = 0;
+        let Weird_Prime = 31;
+        for(let i= 0; i<Math.min(key.length,100); i++){
+            let char = key[i];
+            let value = char.charCodeAt(0) - 96;
+            total = (total*Weird_Prime + value) % this.keyMap.length;
+        }
+        return total;
+    }
+
+}
--- DataStructure/heaps.js
+++ DataStructure/heaps.js
@@ -1,66 +1,28 @@
-class MaxBinaryHeap {
-  constructor() {
-    this.value = [];
-  }
-
-  insert(element) {
-    this.value.push(element);
-    this.bubbleUp();
-  }
-
-  bubbleUp() {
-    let index = this.value.length - 1;
-    let element = this.value[index];
-
-    while (index > 0) {
-      let parentIndex = Math.floor((index - 1) / 2);
-      let parentValue = this.value[parentIndex];
-      if (element <= parentValue) break;
-      this.value[parentIndex] = element;
-      this.value[index] = parentValue;
-      index = parentIndex;
+class BinaryHeap{
+    constructor(){
+        this.value = [];
     }
-  }
 
-  ExtractMax(){
-    let max = this.value[0];
-    let end = this.value.pop();
-    this.value[0] = end;
-    this.sinkDown();
-    return max;
-
-  }
-  sinkDown(){
-    let idx = 0;
-    let length = this.value.length;
-    let element = this.value[idx];
-
-    while(true){
-        let leftChildIdx = 2 * idx + 1;
-        let rigthChildIdx = 2 * idx + 2;
-        let leftChild,rightChild;
-        let swap = null;
-        if(leftChildIdx < length){
-            leftChild = this.value[leftChildIdx];
-            if(leftChild > element){
-                swap =  leftChildIdx;
-            }
-        }
-        if(rigthChildIdx < length) {
-            rightChild = this.value[rigthChildIdx];
-            if(
-                (swap === null && rightChild>element) ||
-                (swap !== null && rightChild>leftChild)
-            ){
-                swap = rigthChildIdx
-            }
-        }
-
-        if(swap === null) return null;
-        this.value[idx] = this.value[swap];
-        this.value[swap] = element;
-        idx = swap;
-
+    insert(element){
+        this.value.push(element);
+        this.BubbleUp();
     }
-  }
+
+    BubbleUp(){
+        let index = this.value.length -1;
+        let insertValue = this.value[index];
+
+        while(index > 0){
+            let parentIndex = Math.floor((index -1)/2);
+            let parentValue = this.value[parentIndex];
+            
+            if(parentValue > insertValue) break;
+
+            this.value[parentIndex] = insertValue;
+            this.value[index] = parentValue;
+
+
+            index = parentIndex;
+        }
+    }
 }
