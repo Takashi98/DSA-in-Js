b852dbd450aaa6fb2ea316ae623736d5dffd0b6f
Takashi98 <vishalprjpt15@gmail.com>
2025-03-08 03:33:27 +0530

    added remove edge and remove vertices method

--- DataStructure/graph.js
+++ DataStructure/graph.js
@@ -1,29 +1,45 @@
-class Graph {
-  constructor() {
-    this.adjacencyList = {};
+class Graph{
+  constructor(){
+    this.adjenancy = {};
   }
 
-  addVertex(vertex) {
-    if(!this.adjacencyList[vertex]) {
-      this.adjacencyList[vertex] = [];
+  addVertex(vertex){
+    if(!this.adjenancy[vertex]) return this.adjenancy[vertex] = [];
+  }
+
+  addEdge(vertex1, vertex2){
+    this.adjenancy[vertex1].push(vertex2);
+    this.adjenancy[vertex2].push(vertex1);
+  }
+
+  removeEdge(vertex1, vertex2){
+    this.adjenancy[vertex1] = this.adjenancy[vertex1].filter(v => v!== vertex2);
+    this.adjenancy[vertex2] = this.adjenancy[vertex2].filter(v => v!== vertex1);
+  }
+  
+  removeVertex(vertex){
+    while(this.adjenancy[vertex].length){
+      let adjVertex = this.adjenancy[vertex].pop();
+      this.removeEdge(vertex,adjVertex);
     }
-  }
-
-  addEdge(v1, v2){
-    this.adjacencyList[v1].push(v2);
-    this.adjacencyList[v2].push(v1);
+    delete this.adjenancy[vertex];
   }
 }
 
 let graph = new Graph();
 
-graph.addVertex("randi");
-graph.addVertex("hiii");
-graph.addVertex("there")
-console.log(graph);
+graph.addVertex("India");
+graph.addVertex("China");
+graph.addVertex("Newyork");
 
-graph.addEdge("randi","hiii");
-console.log(graph);
+graph.addEdge("India", "China");
+graph.addEdge("China", "Newyork")
 
+graph.removeEdge("India", "China")
+
+graph.removeVertex("India");
+
+
+console.log(graph); 
 
 
